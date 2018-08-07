365e5246bfd436ad798c1d745a080cb501b2d86d
STARTERGATE <32726809+startergate@users.noreply.github.com>
2018-08-08 01:06:55 +0900

    레벨 리스트 완성

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 776, frame: false});
+  mainWindow = new BrowserWindow({width: 1400, height: 800, frame: true});
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
