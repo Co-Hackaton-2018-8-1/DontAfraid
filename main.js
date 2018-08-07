6cf106208ee935e666f7df7594ae550b4f21f8db
STARTERGATE <32726809+startergate@users.noreply.github.com>
2018-08-08 02:08:40 +0900

    Bug Fix

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1400, height: 800, frame: true});
+  mainWindow = new BrowserWindow({width: 1400, height: 800});
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
